33b5679f76764a997bfefb0ba58c6e1b8605a6ae
KYoram <vleugels.yoram@gmail.com>
2024-08-26 11:12:41 +0200

    setup code

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"math/rand"
 	"net/http"
+	"net/http/httptest"
+	"os/exec"
+	"runtime"
+	"strings"
 	"time"
 
 	"github.com/KYoram/pub-server/api"
 	"github.com/KYoram/pub-server/file"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/docgen"
 	"github.com/go-chi/render"
 )
+
+var AdminCode int = rand.New(rand.NewSource(time.Now().UnixNano())).Int()
+var ClientCode int = rand.New(rand.NewSource(time.Now().UnixNano())).Int()
 
 func main() {
 	router := chi.NewRouter()
@@ -25,13 +31,55 @@ func main() {
 
 	router.Use(middleware.Timeout(60 * time.Second))
 
-	router.Handle("/", file.ServeStatic())
-
 	api.SetupApiRoutes(router)
 
-	docgen.PrintRoutes(router)
+	router.Handle("/*", file.ServeStatic())
 
-	fmt.Println("Listening on port 8080")
+	server := httptest.NewServer(router)
+	port := strings.Split(server.URL, ":")[2]
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	fmt.Println("Server starting at " + server.URL)
+	fmt.Printf("Use code %d to log in\n", AdminCode)
+
+	openURL(server.URL)
+
+	err := http.ListenAndServe(":"+port, router)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func openURL(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start", url}
+	case "darwin":
+		cmd = "open"
+		args = []string{url}
+	default:
+		if isWSL() {
+			cmd = "cmd.exe"
+			args = []string{"/c", "start", url}
+		} else {
+			cmd = "xdg-open"
+			args = []string{url}
+		}
+	}
+	if len(args) > 1 {
+		args = append(args[:1], append([]string{""}, args[1:]...)...)
+	}
+	return exec.Command(cmd, args...).Start()
+}
+
+func isWSL() bool {
+	releaseData, err := exec.Command("uname", "-r").Output()
+	if err != nil {
+		return false
+	}
+	return strings.Contains(strings.ToLower(string(releaseData)), "microsoft")
 }
